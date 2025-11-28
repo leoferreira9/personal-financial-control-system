983f2724cd4232adc6fbb3264a32c575741fa192
Leonardo <leobarros_@outlook.com>
2025-11-28 20:33:16 -0300

    chore: initial project setup

---
+++ src/main/java/com/leonardo/Personal/Financial/Control/System/PersonalFinancialControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.Personal.Financial.Control.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancialControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancialControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/Personal/Financial/Control/System/PersonalFinancialControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.Personal.Financial.Control.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancialControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
